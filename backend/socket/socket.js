4d969c35baf7c97ec8d7186f320d6f7d01db0a59
lingesan <sglingesan@gmail.com>
2026-03-10 13:38:52 +0530

    dug2fix

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,9 +5,18 @@ module.exports = (io) => {
     console.log("User connected:", socket.id);
 
     socket.on("joinRoom", (roomId) => {
+
       socket.join(roomId);
-      socket.to(roomId).emit("userJoined");
+
+      const clients = io.sockets.adapter.rooms.get(roomId);
+      const numClients = clients ? clients.size : 0;
+
+      if(numClients > 1){
+        socket.to(roomId).emit("userJoined");
+      }
+
       console.log("User joined room:", roomId);
+
     });
 
     socket.on("offer", (data) => {
@@ -20,18 +29,6 @@ module.exports = (io) => {
 
     socket.on("iceCandidate", (data) => {
       socket.to(data.roomId).emit("iceCandidate", data.candidate);
-    });
-
-    socket.on("codeChange", (data) => {
-      socket.to(data.roomId).emit("codeUpdate", data.code);
-    });
-
-    socket.on("chatMessage", (data) => {
-      io.to(data.roomId).emit("chatMessage", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
     });
 
   });
--- frontend/src/components/VideoCall.jsx
+++ frontend/src/components/VideoCall.jsx
@@ -5,78 +5,95 @@ const socket = io("http://localhost:5000");
 
 function VideoCall({ roomId }) {
 
-const localVideo = useRef();
-const remoteVideo = useRef();
+const localVideo = useRef(null);
+const remoteVideo = useRef(null);
 const peerConnection = useRef(null);
 
-useEffect(() => {
-  startCall();
-}, []);
+useEffect(()=>{
 
-const startCall = async () => {
+initCall();
+
+},[]);
+
+const initCall = async () => {
 
 const stream = await navigator.mediaDevices.getUserMedia({
- video: true,
- audio: true
+ video:true,
+ audio:true
 });
 
 localVideo.current.srcObject = stream;
 
 peerConnection.current = new RTCPeerConnection({
- iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+ iceServers:[{ urls:"stun:stun.l.google.com:19302"}]
 });
 
-stream.getTracks().forEach(track => {
- peerConnection.current.addTrack(track, stream);
+stream.getTracks().forEach(track=>{
+ peerConnection.current.addTrack(track,stream);
 });
 
-peerConnection.current.ontrack = (event) => {
+peerConnection.current.ontrack = (event)=>{
+ console.log("Remote stream received");
  remoteVideo.current.srcObject = event.streams[0];
 };
 
-peerConnection.current.onicecandidate = (event) => {
+peerConnection.current.onicecandidate = (event)=>{
  if(event.candidate){
-  socket.emit("iceCandidate", {
+  socket.emit("iceCandidate",{
    roomId,
-   candidate: event.candidate
+   candidate:event.candidate
   });
  }
 };
 
-socket.emit("joinRoom", roomId);
+socket.emit("joinRoom",roomId);
 
-socket.on("userJoined", async () => {
+socket.on("userJoined",async ()=>{
+
+ console.log("Second user joined");
 
  const offer = await peerConnection.current.createOffer();
+
  await peerConnection.current.setLocalDescription(offer);
 
- socket.emit("offer", { roomId, offer });
+ socket.emit("offer",{roomId,offer});
 
 });
 
-socket.on("offer", async (offer) => {
+socket.on("offer",async (offer)=>{
 
- await peerConnection.current.setRemoteDescription(offer);
+ console.log("Offer received");
+
+ await peerConnection.current.setRemoteDescription(
+  new RTCSessionDescription(offer)
+ );
 
  const answer = await peerConnection.current.createAnswer();
+
  await peerConnection.current.setLocalDescription(answer);
 
- socket.emit("answer", { roomId, answer });
+ socket.emit("answer",{roomId,answer});
 
 });
 
-socket.on("answer", async (answer) => {
+socket.on("answer",async (answer)=>{
 
- await peerConnection.current.setRemoteDescription(answer);
+ console.log("Answer received");
+
+ await peerConnection.current.setRemoteDescription(
+  new RTCSessionDescription(answer)
+ );
 
 });
 
-socket.on("iceCandidate", async (candidate) => {
+socket.on("iceCandidate",async (candidate)=>{
 
- try {
-   await peerConnection.current.addIceCandidate(candidate);
- } catch (err) {
-   console.log(err);
+ try{
+  await peerConnection.current.addIceCandidate(
+   new RTCIceCandidate(candidate)
+  );
+ }catch(err){
+  console.log(err);
  }
 
 });
@@ -89,12 +106,12 @@ return(
 
 <div>
 <h3>Your Camera</h3>
-<video ref={localVideo} autoPlay muted width="300"/>
+<video ref={localVideo} autoPlay muted playsInline width="300"/>
 </div>
 
 <div>
 <h3>Remote Camera</h3>
-<video ref={remoteVideo} autoPlay width="300"/>
+<video ref={remoteVideo} autoPlay playsInline width="300"/>
 </div>
 
 </div>
